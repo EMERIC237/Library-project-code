0c587d54943f96449702b18a906716c03a544960
EMERIC237 <bestvemeric@gmail.com>
2021-05-01 15:15:20 -0400

    Helper function added

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -8,16 +8,27 @@ function sortAccountsByLastName(accounts) {
   );
 }
 
-function getTotalNumberOfBorrows(account, books) {
-  //create an accumulator to record the number of appearances
-  let numberOfBorrows = 0;
+const findABook=(books,toDoWhenMatch) =>{
   books.forEach((book) => {
     book.borrows.forEach((borrow) => {
       if (borrow.id === account.id) {
-        numberOfBorrows += 1;
+        toDoWhenMatch(book,borrow,account);
       }
     });
   });
+}
+
+function getTotalNumberOfBorrows(account, books) {
+  //create an accumulator to record the number of appearances
+  let numberOfBorrows = 0;
+  findABook(books,(book,borrow,account) => numberOfBorrows += 1)
+  // books.forEach((book) => {
+  //   book.borrows.forEach((borrow) => {
+  //     if (borrow.id === account.id) {
+  //       numberOfBorrows += 1;
+  //     }
+  //   });
+  // });
   return numberOfBorrows;
 }
 
@@ -25,23 +36,33 @@ function getBooksPossessedByAccount(account, books, authors) {
   //create a list to hold all the books
   let BooksPossessedByAccount = [];
   //Go through all the books inside the array
-  books.forEach((book) => {
-    //Go through borrow array inside the book
-    book.borrows.forEach((borrow) => {
-      //check if the id is equal to the account id
-      if (borrow.id === account.id) {
-        //add the book to the initial array if returned === false
-        if (!borrow.returned) {
-          //add the author to the books inside the array.
-          let bookAuthor = authors.find(
-            (author) => author.id === book.authorId
-          );
-          let newBook = { ...book, author: bookAuthor };
-          BooksPossessedByAccount[BooksPossessedByAccount.length] = newBook;
-        }
-      }
-    });
-  });
+  findABook(books,(book,borrow,account) => {
+    if (!borrow.returned) {
+      //add the author to the books inside the array.
+      let bookAuthor = authors.find(
+        (author) => author.id === book.authorId
+      );
+      let newBook = { ...book, author: bookAuthor };
+      BooksPossessedByAccount[BooksPossessedByAccount.length] = newBook;
+    }
+  })
+  // books.forEach((book) => {
+  //   //Go through borrow array inside the book
+  //   book.borrows.forEach((borrow) => {
+  //     //check if the id is equal to the account id
+  //     if (borrow.id === account.id) {
+  //       //add the book to the initial array if returned === false
+  //       // if (!borrow.returned) {
+  //       //   //add the author to the books inside the array.
+  //       //   let bookAuthor = authors.find(
+  //       //     (author) => author.id === book.authorId
+  //       //   );
+  //       //   let newBook = { ...book, author: bookAuthor };
+  //       //   BooksPossessedByAccount[BooksPossessedByAccount.length] = newBook;
+  //       // }
+  //     }
+  //   });
+  // });
 
   return BooksPossessedByAccount;
 }
